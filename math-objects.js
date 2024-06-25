7405db887483b48bc5212c642509c11714fc7af8
Vismay Malviya <90767593+vismaymalviya@users.noreply.github.com>
2024-06-25 18:06:38 +0530

    Add files via upload

---
+++ math-objects.js
@@ -0,0 +1,15 @@
+//STATIC PROPERTIES
+Math.E        // returns Euler's number
+Math.PI       // returns PI
+Math.SQRT2    // returns the square root of 2
+Math.SQRT1_2  // returns the square root of 1/2
+Math.LN2      // returns the natural logarithm of 2
+Math.LN10     // returns the natural logarithm of 10
+Math.LOG2E    // returns base 2 logarithm of E
+Math.LOG10E   // returns base 10 logarithm of E
+
+//STATIC METHODS
+console.log(Math.ceil(4.76))	//Returns x rounded up to its nearest integer
+console.log(Math.round(4.76))	//Returns x rounded to its nearest integer
+console.log(Math.floor(4.76))	//Returns x rounded down to its nearest integer
+console.log(Math.trunc(4.76))	//Returns the integer part of x (new in ES6)
