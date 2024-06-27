71b0862e4f7ab89ec33be05a0b802bf25989d885
Vismay Malviya <90767593+vismaymalviya@users.noreply.github.com>
2024-06-27 16:48:49 +0530

    Add files via upload

---
+++ events-in-JS.js
@@ -0,0 +1,9 @@
+// Get a reference to the button element
+var button = document.getElementById('myButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', function() {
+    // Code to execute when the button is clicked
+    console.log(2 + 2);
+    alert('Button clicked!');
+});
